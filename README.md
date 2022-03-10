1d4d524ad681d683474c2974ab1e9442ba026031
Joshua <joshuagpatten@gmail.com>
2022-03-10 13:50:57 -0500

    Complete basic UI for rps game

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Odin Project RPS game for JS foundations
 
 This is my first JS project hope it goes well!
+
+On branch rps-ui, I am creating a user interface for this rock paper scissors game whereas it was played in the console previously.
--- myscripts.js
+++ myscripts.js
@@ -2,14 +2,16 @@ const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 let playerScore = 0;
 let computerScore = 0;
+document.getElementById("playerScore").textContent = playerScore;
+document.getElementById("computerScore").textContent = computerScore;
+
 //Returns string which is computer's random choice from the choices array
 function computerPlay(){
     let choice = choices[Math.floor(Math.random()*choices.length)];
     return choice;
 }
 
-function RPSRound(playerSelection, computerSelection=null){
-    
+/*function playRound(playerSelection, computerSelection=null){
     computerSelection = computerPlay();
     while(1){
         if(typeof playerSelection === "string"){
@@ -36,11 +38,89 @@ function RPSRound(playerSelection, computerSelection=null){
     }       
 
     else{console.log("You didn't enter a valid choice, round skipped.")}
+}*/
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+       let computerSelection = computerPlay();
+        let playerSelection = button.id;
+        while(1){
+            if(typeof playerSelection === "string"){
+                playerSelection = playerSelection.toUpperCase();
+                break;
+            }
+            else{console.log("Please only use string");}
+        }
+        
+        if(playerSelection === computerSelection){
+            console.log(`Player chose: ${playerSelection} \nComputer chose: ${computerSelection} \n It's a tie!`);        //tie
+        }
+        else if ((computerSelection ==="PAPER" && playerSelection === "ROCK") || 
+        (computerSelection ==="SCISSORS" && playerSelection === "PAPER") || 
+        (computerSelection ==="ROCK" && playerSelection === "SCISSORS")){
+            computerScore++;
+            console.log(`Player chose: ${playerSelection} \nComputer chose: ${computerSelection} \nComputer wins!`);       //comp wins
+        }
+        else if((computerSelection ==="SCISSORS" && playerSelection === "ROCK") ||
+        (computerSelection ==="ROCK" && playerSelection === "PAPER") ||
+        (computerSelection ==="PAPER" && playerSelection === "SCISSORS")){
+            playerScore++;
+            console.log(`Player chose: ${playerSelection} \nComputer chose: ${computerSelection} \nPlayer wins!`);      //play wins
+        }       
+    
+        else{console.log("You didn't enter a valid choice, round skipped.")}
+        document.getElementById("playerScore").textContent = playerScore;
+        document.getElementById("computerScore").textContent = computerScore;
+        if(playerScore===5 ){
+            document.write("YOU WIN!!!!!!!!!");
+        }
+        else if(computerScore===5 ){
+            document.write("Computer wins, better luck next time!");
+        }
+
+    });
+  });
+
+function playRound(){
+    
+    let computerSelection = computerPlay();
+    let playerSelection = String(buttons.id);
+    while(1){
+        if(typeof playerSelection === "string"){
+            playerSelection = playerSelection.toUpperCase();
+            break;
+        }
+        else{console.log("Please only use string");}
+    }
+    
+    if(playerSelection === computerSelection){
+        console.log(`Player chose: ${playerSelection} \nComputer chose: ${computerSelection} \n It's a tie!`);        //tie
+    }
+    else if ((computerSelection ==="PAPER" && playerSelection === "ROCK") || 
+    (computerSelection ==="SCISSORS" && playerSelection === "PAPER") || 
+    (computerSelection ==="ROCK" && playerSelection === "SCISSORS")){
+        computerScore++;
+        console.log(`Player chose: ${playerSelection} \nComputer chose: ${computerSelection} \nComputer wins!`);       //comp wins
+    }
+    else if((computerSelection ==="SCISSORS" && playerSelection === "ROCK") ||
+    (computerSelection ==="ROCK" && playerSelection === "PAPER") ||
+    (computerSelection ==="PAPER" && playerSelection === "SCISSORS")){
+        playerScore++;
+        console.log(`Player chose: ${playerSelection} \nComputer chose: ${computerSelection} \nPlayer wins!`);      //play wins
+    }       
+
+    else{console.log("You didn't enter a valid choice, round skipped.")}
+   
 }
 
-function game(){
+
+
+
+/*function game(){
     for(let i = 0; i < 5; i++){
-        RPSRound(playerSelection = prompt("Enter rock, paper, or scissors"), null);
+        playRound(playerSelection = prompt("Enter rock, paper, or scissors"), null);
     }
     console.log(`The final score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`);
     if(playerScore > computerScore){
@@ -53,3 +133,4 @@ function game(){
 }
 
 game();
+*/
